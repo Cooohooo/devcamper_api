6af4b781eaa5e0f8566763d610b0d9dcd7588579
weihong he <weihhe@cisco.com>
2025-06-25 15:58:58 +0800

    feat: update & delete course

--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,10 @@
 const express = require("express");
 // * Load controller
-const { getCourses, getCourse, addCourse } = require("../controller/courses");
+const { getCourses, getCourse, addCourse, updateCourse, deleteCourse } = require("../controller/courses");
 
 const router = express.Router({ mergeParams: true });
 
 router.route("/").get(getCourses).post(addCourse);
-router.route("/:id").get(getCourse);
+router.route("/:id").get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
