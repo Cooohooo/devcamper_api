5fa1d3e68d4bc5f9e166849130e9d1475489d1ad
weihong he <weihhe@cisco.com>
2025-07-09 15:46:12 +0800

    feat: password encrypt

--- controller/auth.js
+++ controller/auth.js
@@ -6,11 +6,16 @@ const User = require("../models/User");
 // * @route GET /api/v1/auth/register
 // * @access Public
 const register = asyncHandler(async (req, res) => {
-    const { name, email, password } = req.body;
+    const { name, email, password, role } = req.body;
 
-    res.status(200).json(res.advancedResults);
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role,
+    });
+
+    res.status(200).json({ success: true, data: user });
 });
 
-exports.register = asyncHandler(async (req, res) => {
-    res.status(200).json({ success: true, data: "User registered" });
-});
+exports.register = register;
